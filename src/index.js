e8751f88cb2525507a81db1b52659cde3f4ec55f
Eden Castillo Asport <35803612+phenomenone@users.noreply.github.com>
2022-04-28 16:43:46 -0500

    add div

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+
+  document.getElementById("root")
+);
